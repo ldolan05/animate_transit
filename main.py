dcaefac7cfc76d8e2c0216f2907ca178cbf42bf5
Lucy Dolan <ldolan05@qub.ac.uk>
2023-07-11 14:14:17 -0500

    added limb darkening to object 2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from glob import glob
 
 class system(object):
 
-    def __init__(self, n_pixs, R1, R2, a_R1, b, theta, L2_L1, u1=None, u2=None):
+    def __init__(self, n_pixs, R1, R2, a_R1, b, theta, L2_L1, u1_1=None, u2_1=None, u1_2=None, u2_2=None):
         self.n_pixs = n_pixs    # number of pixels in grid (npixs by npixs)
 
         pix_per_unit = n_pixs/(3*R1+2*(a_R1*R1)+3*R2)  #conversion factor between pixels and unit
@@ -19,8 +19,11 @@ class system(object):
         self.grid1 = np.zeros((self.n_pixs, self.n_pixs))
         self.L2_L1 = L2_L1
 
-        self.u1 = u1
-        self.u2 = u2
+        self.u1_1= u1_1
+        self.u2_1 = u2_1
+
+        self.u1_2= u1_2
+        self.u2_2 = u2_2
 
     def model_object1(self):
         
@@ -31,10 +34,10 @@ class system(object):
                 # calculate pixel distance from centre of object 1
                 r = np.sqrt((x-centre_x)**2 + (y-centre_y)**2)
                 if r <= self.R1:
-                    if self.u1 != None and self.u2 != None:
+                    if self.u1_1 != None and self.u2_1 != None:
                         # calculate flux using quadratic limb darkening
                         onemu=1.-np.sqrt(1.-r**2/(self.R1)**2)
-                        self.grid1[y, x]=1.-self.u1*onemu - self.u2*onemu*onemu
+                        self.grid1[y, x]=1.-self.u1_1*onemu - self.u2_1*onemu*onemu
                     else:
                         self.grid1[y, x]=1.
 
@@ -52,10 +55,11 @@ class system(object):
                 # calculate distance of pixel from centre of object 2
                 r = np.sqrt((x-x1)**2 + (y-y1)**2)
                 if r <= self.R2:
-                    if abs(phase)<0.25:
+                    if abs(phase)<0.25 or self.grid1[y, x]==0:
                         grid[y, x]=self.L2_L1
-                    elif self.grid1[y, x]==0:
-                        grid[y, x]=self.L2_L1
+                        if self.u1_2!=None and self.u2_2!=None:
+                            onemu=1.-np.sqrt(1.-r**2/(self.R2)**2)
+                            grid[y, x]=grid[y, x]*(1-self.u1_2*onemu - self.u2_2*onemu*onemu)
 
         return grid
 
@@ -66,7 +70,7 @@ def gif_maker(file_list):
             writer.append_data(image)
 
 
-test_system = system(n_pixs=1000, R1=0.32, R2=0.11, a_R1=12.7, b=0.8, theta=0, L2_L1=0.1, u1 = 2 * np.sqrt(0.6) * 0.85, u2=np.sqrt(0.6) * (1 - 2 * 0.85))
+test_system = system(n_pixs=1000, R1=0.32, R2=0.11, a_R1=12.7, b=0.8, theta=0, L2_L1=0.9, u1_1 = 2 * np.sqrt(0.6) * 0.85, u2_1=np.sqrt(0.6) * (1 - 2 * 0.85), u1_2 = 2 * np.sqrt(0.6) * 0.85, u2_2=np.sqrt(0.6) * (1 - 2 * 0.85))
 
 test_system.model_object1()
 
